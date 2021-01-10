f74dc1338389a99700ed5fe3d484eedc56b91404
ujjwalbb30 <58893950+ujjwalbb30@users.noreply.github.com>
2021-01-10 22:05:40 +0000

    Add files via upload

--- entry_script.py
+++ entry_script.py
@@ -1,7 +1,8 @@
 import json
 import numpy as np
+import pandas as pd
 import os
-from sklearn.externals import joblib
+import joblib
 
 
 def init():
@@ -14,7 +15,8 @@ def init():
 
 def run(data):
     try:
-        data = np.array(json.loads(data))
+        data = json.loads(data)['data']
+        data = pd.DataFrame.from_dict(data)
         result = model.predict(data)
         return result.tolist()
     except Exception as e:
